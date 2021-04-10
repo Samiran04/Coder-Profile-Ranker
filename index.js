371121366c1857348193f460124f6867f492cdd4
Samiran Roy <samiranroy0407@gmail.com>
2021-04-11 03:58:39 +0530

    home page added

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use(express.urlencoded());
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use('/', require('./routes'));
+
 app.listen(port, function(err){
     if(err){console.log('Error while running server', err); return;}
 });
--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,6 @@ const router = express.Router();
 
 const home_controller = require('../controllers/home_controller');
 
+router.get('/', home_controller.home);
+
 module.exports = router;
