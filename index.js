9fdce7989e77ceb9e221eb3fc8ace46efef68f4a
Samiran Roy <samiranroy0407@gmail.com>
2021-04-11 03:54:58 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.use(express.urlencoded());
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.listen(port, function(err){
+    if(err){console.log('Error while running server', err); return;}
+});
